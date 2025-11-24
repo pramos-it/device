565c5babebf58d84c7b9d95901f4fefc3d3ae90f
pramos-it <pramos.it@gmail.com>
2025-11-24 10:10:34 -0400

    update toggle

--- src/main/java/com/pramos/devices/config/SecurityConfig.java
+++ src/main/java/com/pramos/devices/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedOrigins(allowedOrigins)
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*").allowCredentials(true)
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH").allowedHeaders("*").allowCredentials(true)
 				.maxAge(3600);
 	}
 }
